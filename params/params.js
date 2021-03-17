e916906d801fbba84541179fa6e6e8af99ec304a
ArvinBahrami <47042142+ArvinBahrami@users.noreply.github.com>
2021-03-17 12:02:55 -0500

    first commit

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://Ipro497:StudyRoom2021@cluster0.6agwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
